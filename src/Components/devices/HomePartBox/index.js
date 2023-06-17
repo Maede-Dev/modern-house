f9511189003a80e5f982f9f05de5192960f5cf47
maede <maedekarimi69110@gmail.com>
2023-06-17 15:22:13 +0330

    handle modal in devices page

--- src/Components/devices/HomePartBox/index.js
+++ src/Components/devices/HomePartBox/index.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./HomePartBox.module.scss";
+import ReactModal from "react-modal";
 const HomePartBox = () => {
+  const [visible, setVisible] = useState(false);
   const boxList = [
     {
       id: 1,
       title: "Living",
       href: "/images/living-room.png",
-      className: s.f01,
+      className: s.Living,
       width: "200px",
       height: "200px",
     },
@@ -14,7 +16,7 @@ const HomePartBox = () => {
       id: 2,
       title: "Kitchen",
       href: "/images/refrigerator.png",
-      className: s.f01,
+      className: s.Kitchen,
       width: "200px",
       height: "200px",
     },
@@ -22,7 +24,7 @@ const HomePartBox = () => {
       id: 3,
       title: "Bedroom",
       href: "/images/bedroom.png",
-      className: s.f01,
+      className: s.Bedroom,
       width: "200px",
       height: "200px",
     },
@@ -30,20 +32,33 @@ const HomePartBox = () => {
       id: 4,
       title: "bathroom",
       href: "/images/washing-machine.png",
-      className: s.f01,
+      className: s.bathroom,
       width: "200px",
       height: "200px",
     },
   ];
+
   return (
-    <div className={s.HomePartBox_container}>
-      {boxList?.map((item) => (
-        <div className={s.HomePartBox_Item} key={item?.id}>
-          <img src={item.href} alt="icon" className={s.img} />
-          <div className={s.title}>{item.title}</div>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className={s.HomePartBox_container}>
+        {boxList?.map((item) => (
+          <div className={s.HomePartBox_Item} key={item?.id}>
+            <img src={item.href} alt="icon" className={s.img} />
+            <div className={s.title}>{item.title}</div>
+          </div>
+        ))}
+      </div>
+      <ReactModal
+        ariaHideApp={false}
+        className={`${s.modal} ${s.modalBody}`}
+        overlayClassName={s.overlay}
+        isOpen={visible}
+        onRequestClose={() => setVisible(false)}
+        htmlOpenClassName={s.modalBox}
+      >
+        <div className={s.title}>bulb</div>
+      </ReactModal>
+    </>
   );
 };
 
